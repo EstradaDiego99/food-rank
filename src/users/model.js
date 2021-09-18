a606f0621bb03a2eddb9451dd584551123c394bb
Diego Estrada Talamantes <devdiegoest@gmail.com>
2021-09-18 09:07:10 -0500

    Creation of user model

---
+++ src/users/model.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require("mongoose");
+
+const schema = new Schema({
+  /** Name of the user. */
+  name: {
+    type: String,
+    required: [true, "The name is necessary."],
+  },
+
+  /** Refers to the user email registration. */
+  email: {
+    type: String,
+    required: [true, "The email is necessary."],
+  },
+
+  /** Hash of the user password. */
+  password: {
+    type: String,
+    required: [true, "The password hash is necessary."],
+  },
+
+  /** ELO for each dish for this particular user. */
+  dishesELO: {
+    type: Map, // { "61459a71cd9e5c1b2543069b":1456}
+    of: Number,
+    required: false,
+  },
+});
+
+/** Mongoose model for user. */
+const User = model("User", schema);
+
+module.exports = User;
