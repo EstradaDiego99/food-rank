00cd5e4341c10d6886a3a1f1e86ca6397192dee0
NicoleBarra <nicolasabarra@hotmail.com>
2021-09-19 00:08:25 -0500

    dishes list show index

---
+++ src/dishes/views/dishCard.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./styles.css";
+
+export default function DishCard (props) {
+
+  const [tags, setTags] = useState();
+
+
+  useEffect(()=>{
+   setTags(props.tags)
+  },[props.tags]);
+
+  return ( 
+  <Link className="link" to={`/dishes/${props.id}`}>
+   <div className="Restaurant-card border-radius-1">
+      <div className="arrange-card">
+        <div className="left-card">
+              <h3>{props.name}</h3>
+          {tags?
+            <div className="tags">
+              {tags.map((tag,index)=>{
+              return(
+                <div>
+                {index <5 && 
+                  <div>
+                    {(index%2 == 0)? 
+                      <p className="tag primary" key="tag">{tag}</p>
+                      :
+                      <p className="tag secondary" key="tag">{tag}</p>
+                    }
+                  </div>
+                }
+                </div>
+              ) 
+          
+            })}
+          </div>
+          :
+          <></>
+          }
+          <div className="address-res-card">
+            <p>
+              {props.address} {props.city} {props.state} {props.postalCode}
+            </p>
+          </div>
+        </div>
+        <div className="image-res-card">
+          <img className="image-res" src={props.photo}></img>
+        </div>
+      </div>
+    </div>
+  </Link>
+   
+  )
+}
