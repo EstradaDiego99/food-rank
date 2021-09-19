396e50d7033abbf29c24298b5c47f43bd0151e56
Diego Estrada Talamantes <devdiegoest@gmail.com>
2021-09-19 06:42:10 -0500

    Add production baseURL

--- src/utils/customAxios.js
+++ src/utils/customAxios.js
@@ -3,5 +3,5 @@ import axios from "axios";
 export default axios.create(
   process.env.NODE_ENV === "development"
     ? { baseURL: "http://localhost:5000/api" }
-    : {}
+    : { baseURL: "https://yumyum-rank.herokuapp.com/api" }
 );
