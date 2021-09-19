b69185c9fbf2fd5f5a66a23ea983e24763f058f2
Diego Estrada Talamantes <devdiegoest@gmail.com>
2021-09-19 04:27:06 -0500

    Add restaurant location to dish card

--- src/dishes/views/dishCard.js
+++ src/dishes/views/dishCard.js
@@ -2,55 +2,61 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./styles.css";
 
-export default function DishCard (props) {
+import iconPlace from "../../assets/place_black_24dp.svg";
 
+export default function DishCard(props) {
   const [tags, setTags] = useState();
 
+  useEffect(() => {
+    setTags(props.tags);
+  }, [props.tags]);
 
-  useEffect(()=>{
-   setTags(props.tags)
-  },[props.tags]);
-
-  return ( 
-  <Link className="link" to={`/dishes/${props.id}`}>
-   <div className="Restaurant-card border-radius-1">
-      <div className="arrange-card">
-        <div className="left-card">
-              <h3>{props.name}</h3>
-          {tags?
-            <div className="tags">
-              {tags.map((tag,index)=>{
-              return(
-                <div>
-                {index <5 && 
-                  <div>
-                    {(index%2 == 0)? 
-                      <p className="tag green" key="tag">{tag}</p>
-                      :
-                      <p className="tag blue" key="tag">{tag}</p>
-                    }
-                  </div>
-                }
-                </div>
-              ) 
-          
-            })}
+  return (
+    <Link className="link" to={`/dishes/${props.id}`}>
+      <div className="Restaurant-card border-radius-1">
+        <div className="arrange-card">
+          <div className="left-card">
+            <h3 className="mb-1">{props.name}</h3>
+            <div className="d-flex mb-2">
+              <img src={iconPlace} alt="" style={{ height: "1.5em" }} />
+              <h6>{props.restaurant.name}</h6>
+            </div>
+            {tags ? (
+              <div className="tags">
+                {tags.map((tag, index) => {
+                  return (
+                    <div>
+                      {index < 5 && (
+                        <div>
+                          {index % 2 === 0 ? (
+                            <p className="tag green" key="tag">
+                              {tag}
+                            </p>
+                          ) : (
+                            <p className="tag blue" key="tag">
+                              {tag}
+                            </p>
+                          )}
+                        </div>
+                      )}
+                    </div>
+                  );
+                })}
+              </div>
+            ) : (
+              <></>
+            )}
+            <div className="address-res-card">
+              <h5>
+                ${props.price} {props.currency}
+              </h5>
+            </div>
           </div>
-          :
-          <></>
-          }
-          <div className="address-res-card">
-            <h5>
-              ${props.price} {props.currency} 
-            </h5>
+          <div className="image-res-card">
+            <img className="image-res" src={props.photo} alt=""></img>
           </div>
-        </div>
-        <div className="image-res-card">
-          <img className="image-res" src={props.photo}></img>
         </div>
       </div>
-    </div>
-  </Link>
-   
-  )
+    </Link>
+  );
 }
